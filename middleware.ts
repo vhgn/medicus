e8aaac1c62d34fd827bac6166498abe506aeb3f6
Vahagn <metavahagn@icloud.com>
2025-06-21 21:25:58 +0400

    feat: add debug logging

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,26 @@
 import {
-  convexAuthNextjsMiddleware,
-  createRouteMatcher,
-  nextjsMiddlewareRedirect,
-} from "@convex-dev/auth/nextjs/server";
+	convexAuthNextjsMiddleware,
+	createRouteMatcher,
+	nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server"
 
-const isSignInPage = createRouteMatcher(["/signin"]);
-const isProtectedRoute = createRouteMatcher(["/", "/server"]);
+const isSignInPage = createRouteMatcher(["/signin"])
+const isProtectedRoute = createRouteMatcher(["/"])
 
-export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
-  if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
-    return nextjsMiddlewareRedirect(request, "/");
-  }
-  if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
-    return nextjsMiddlewareRedirect(request, "/signin");
-  }
-});
+export default convexAuthNextjsMiddleware(
+	async (request, { convexAuth }) => {
+		if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
+			return nextjsMiddlewareRedirect(request, "/")
+		}
+		if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
+			return nextjsMiddlewareRedirect(request, "/signin")
+		}
+	},
+	{ verbose: Boolean(process.env.DEBUG) },
+)
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+	// The following matcher runs middleware on all routes
+	// except static assets.
+	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+}
