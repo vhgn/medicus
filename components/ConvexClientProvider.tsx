39cf86bf3f1ce186219f5878aaa7ec64ea9bba1b
Vahagn <metavahagn@icloud.com>
2025-06-24 01:19:14 +0400

    feat: add verbose logging

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -1,19 +1,21 @@
-"use client";
+"use client"
 
-import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
-import { ConvexReactClient } from "convex/react";
-import { ReactNode } from "react";
+import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs"
+import { ConvexReactClient } from "convex/react"
+import { ReactNode } from "react"
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
+	verbose: Boolean(process.env.DEBUG),
+})
 
 export default function ConvexClientProvider({
-  children,
+	children,
 }: {
-  children: ReactNode;
+	children: ReactNode
 }) {
-  return (
-    <ConvexAuthNextjsProvider client={convex}>
-      {children}
-    </ConvexAuthNextjsProvider>
-  );
+	return (
+		<ConvexAuthNextjsProvider client={convex}>
+			{children}
+		</ConvexAuthNextjsProvider>
+	)
 }
